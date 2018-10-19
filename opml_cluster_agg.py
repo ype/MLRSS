1af502883d6fc873548033a4e7bb113c12211b22
Anton Strilchuk <strilchu@adobe.com>
2018-10-19 13:45:16 +0100

    Fixes #2

--- opml_cluster_agg.py
+++ opml_cluster_agg.py
@@ -44,6 +44,31 @@ feeds = ['http://feeds.feedburner.com/uk/gizmodo?format=xml',
 ##`----
 import feedparser
 import nltk
+import re
+
+def clean_html(html):
+    """
+    Copied from NLTK package.
+    Remove HTML markup from the given string.
+
+    :param html: the HTML string to be cleaned
+    :type html: str
+    :rtype: str
+    """
+
+    # First we remove inline JavaScript/CSS:
+    cleaned = re.sub(r"(?is)<(script|style).*?>.*?(</\1>)", "", html.strip())
+    # Then we remove html comments. This has to be done before removing regular
+    # tags since comments can contain '>' characters.
+    cleaned = re.sub(r"(?s)<!--(.*?)-->[\n]?", "", cleaned)
+    # Next we can remove the remaining tags:
+    cleaned = re.sub(r"(?s)<.*?>", " ", cleaned)
+    # Finally, we deal with whitespace
+    cleaned = re.sub(r"&nbsp;", " ", cleaned)
+    cleaned = re.sub(r"  ", " ", cleaned)
+    cleaned = re.sub(r"  ", " ", cleaned)
+    return cleaned.strip()
+
 corpus = []
 titles=[]
 hyperlinks=[]
@@ -53,7 +78,7 @@ for feed in feeds:
     for e in d['entries']:
         #working on function to constrain dates to specified time range
         #if e['published'] <= "Fri, 04 Apr 2014":
-            words = nltk.wordpunct_tokenize(nltk.clean_html(e['description']))
+            words = nltk.wordpunct_tokenize(clean_html(e['description']))
             words.extend(nltk.wordpunct_tokenize(e['title']))
             lowerwords=[x.lower() for x in words if len(x) > 1]
             ct += 1
